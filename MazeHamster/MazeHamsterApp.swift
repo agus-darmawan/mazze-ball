042cd9e08847389592a98d2ce952c9105759ab22
alizaenazet <alizaenazet@icloud.com>
2025-07-15 02:58:11 +0700

    Initial Commit

---
+++ MazeHamster/MazeHamsterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MazeHamsterApp.swift
+//  MazeHamster
+//
+//  Created by Ali zaenal on 15/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MazeHamsterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
